f530a9ef0b194f80141d142c076b6419809763ce
makezh <max.kezhaev@gmail.com>
2022-10-12 12:53:02 +0300

    add homework v1.1.0

--- 04/Metaclass.py
+++ 04/Metaclass.py
@@ -1,10 +1,14 @@
 class CustomMeta(type):
-    def __new__(mcs, name, bases, dct):
+    def __new__(cls, name_cls, bases, dct):
         changing_name = 'custom_'
-        attrs = ((name, value) for name, value in dct.items() if not name.startswith('__'))
-        changed_dct = dict((changing_name + name, value) for name, value in attrs)
+        new_dct = {}
+        for name, value in dct.items():
+            if not name.startswith('__'):
+                new_dct[changing_name + name] = value
+            else:
+                new_dct[name] = value
 
-        return super(CustomMeta, mcs).__new__(mcs, name, bases, changed_dct)
+        return super(CustomMeta, cls).__new__(cls, name_cls, bases, new_dct)
 
 
 class CustomClass(metaclass=CustomMeta):
@@ -14,46 +18,10 @@ class CustomClass(metaclass=CustomMeta):
         self.val = val
 
     def line(self):
-        return self.__dir__()
+        return 'Line: ' + str(self)
 
     def __str__(self):
         return "Custom_by_metaclass"
 
-    def __getattribute__(self, item):
-        print(f"getattribute {item}")
-        return type.__getattribute__(item)
-
     def __setattr__(self, key, value):
-        print(f"setattr {key} = {value}")
-        return type.__setattr__("custom" + key, value)
-
-    def __delattr__(self, item):
-        print(f"delattr {item}")
-        return type.__delattr__(item)
-
-    def __getattr__(self, item):
-        print(f"getattr {item}")
-        return type.__getattr__(item)
-
-
-inst = CustomClass()
-inst.__setattr__("p", "q")
-print(inst.__dict__.items())
-print(inst.__dir__())
-print(hasattr(inst, 'custom_val'))
-'''
-inst.custom_x == 50
-inst.custom_val == 99
-inst.custom_line() == 100
-CustomClass.custom_x == 50
-str(inst) == "Custom_by_metaclass"
-
-inst.dynamic = "added later"
-inst.custom_dynamic == "added later"
-inst.dynamic  # ошибка
-
-inst.x  # ошибка
-inst.val  # ошибка
-inst.line()  # ошибка
-inst.yyy  # ошибка
-CustomClass.x  # ошибка'''
+        self.__dict__[f"custom_{key}"] = value
--- 04/metaclass_tests.py
+++ 04/metaclass_tests.py
@@ -1,9 +1,31 @@
 import unittest
+from Metaclass import CustomClass
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_integrated_attrs(self):
+        inst = CustomClass()
+        self.assertEqual(hasattr(inst, 'x'), False)
+        self.assertEqual(hasattr(inst, 'val'), False)
+        self.assertEqual(hasattr(inst, 'line'), False)
+
+        self.assertEqual(hasattr(inst, 'custom_val'), True)
+        self.assertEqual(inst.custom_val, 99)
+
+        self.assertEqual(hasattr(inst, 'custom_x'), True)
+        self.assertEqual(inst.custom_x, 50)
+
+        self.assertEqual(hasattr(inst, 'custom_line'), True)
+        self.assertEqual(inst.custom_line(), 'Line: Custom_by_metaclass')
+
+        self.assertEqual(str(inst), 'Custom_by_metaclass')
+
+    def test_new_attrs(self):
+        inst = CustomClass()
+        inst.test = 'testing...'
+        self.assertEqual(hasattr(inst, 'test'), False)
+        self.assertEqual(hasattr(inst, 'custom_test'), True)
+        self.assertEqual(inst.custom_test, 'testing...')
 
 
 if __name__ == '__main__':
