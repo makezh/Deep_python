2d18cb5c1f2637c6be755eecbca7b83cbfc3f735
makezh <max.kezhaev@gmail.com>
2022-11-17 04:08:42 +0300

    1. Change module name 2. Change logic of CustomList's work 3. Add new tests for immutability of lists 4. correct comparison of lists 5. Add new tests for additions

---
+++ 03/CustomList.py
@@ -0,0 +1,76 @@
+from itertools import zip_longest
+
+
+class CustomList(list):
+    def __init__(self, *args):
+        if len(args) == 0:
+            super().__init__([])
+        else:
+            super().__init__(args[0])
+
+    def __add__(self, other):
+        return CustomList([sum(i) for i in zip_longest(self,
+                                                       other,
+                                                       fillvalue=0)])
+
+    def __sub__(self, other):
+        return CustomList([sum(i) for i in
+                           zip_longest(self,
+                                       [-number for number in other],
+                                       fillvalue=0)])
+
+    def __radd__(self, other):
+        return CustomList([sum(i) for i in zip_longest(other,
+                                                       self,
+                                                       fillvalue=0)])
+
+    def __rsub__(self, other):
+        return CustomList([sum(i) for i in
+                           zip_longest(other,
+                                       [-number for number in self],
+                                       fillvalue=0)])
+
+    def __lt__(self, other):
+        return sum(self) < sum(other)
+
+    def __le__(self, other):
+        return sum(self) <= sum(other)
+
+    def __gt__(self, other):
+        return sum(self) > sum(other)
+
+    def __ge__(self, other):
+        return sum(self) >= sum(other)
+
+    def __eq__(self, other):
+        return sum(self) == sum(other)
+
+    def __ne__(self, other):
+        return sum(self) != sum(other)
+
+    def __repr__(self):
+        return "CustomList(" + super().__repr__() + ")"
+
+    def __str__(self):
+        return super().__str__() + f" sum = {sum(self)};"
+
+
+def main():
+    if __name__ == '__main__':
+        sub = CustomList([5, 1, 3, 7]) - CustomList([1, 2, 7])
+        add = CustomList([5, 1, 3, 7]) + CustomList([1, 2, 7])
+        print(f"Only customs\nADD = {add}\n"
+              f"SUB = {sub}\n")
+        add_list = CustomList([5, 1, 3, 7]) + [1, 2, 7]
+        sub_list = CustomList([5, 1, 3, 7]) - [1, 2, 7]
+        print(f"Custom +- list\nADD = {add_list}\n"
+              f"SUB = {sub_list}\n")
+        add_list_r = [1, 2, 7] + CustomList([5, 1, 3, 7])
+        sub_list_r = [1, 2, 7] - CustomList([5, 1, 3, 7])
+        print(f"List +- custom\nADD = {add_list_r}\n"
+              f"SUB = {sub_list_r}\n")
+        return "end of main"
+    return "just end"
+
+
+main()
--- 03/tests.py
+++ 03/tests.py
@@ -1,7 +1,12 @@
+import functools
 import unittest
 from unittest.mock import patch
 
-from main import CustomList, main
+from CustomList import CustomList, main
+
+
+def list_comparison(list_1, list_2) -> bool:
+    return functools.reduce(lambda x, y: x and y, map(lambda p, q: p == q, list_1, list_2), True)
 
 
 class MyTestCase(unittest.TestCase):
@@ -10,9 +15,9 @@ class MyTestCase(unittest.TestCase):
         list_1 = CustomList([1, 2, 3])
         list_2 = CustomList([])
         list_3 = CustomList()
-        self.assertEqual(str(list_1), '[1, 2, 3] sum = 6;')
-        self.assertEqual(str(list_2), '[] sum = 0;')
-        self.assertEqual(str(list_3), '[] sum = 0;')
+        self.assertEqual(str(list_1), 'CustomList([1, 2, 3]) sum = 6;')
+        self.assertEqual(str(list_2), 'CustomList([]) sum = 0;')
+        self.assertEqual(str(list_3), 'CustomList([]) sum = 0;')
         self.assertEqual(repr(list_1), 'CustomList([1, 2, 3])')
 
     def test_len(self):
@@ -29,22 +34,56 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(list_1 == list_2, False)
         self.assertEqual(list_1 != list_2, True)
 
-    def test_adds(self):
-        list_1 = CustomList([5, 1, 3, 7])
-        list_2 = CustomList([1, 2, 7])
-        simple_list = [1, 2, 3]
-        self.assertEqual(list_1 + list_2, CustomList([6, 3, 10, 7]))
-        self.assertEqual(list_1 - list_2, CustomList([4, -1, -4, 7]))
-        self.assertEqual(list_1 - simple_list, CustomList([4, -1, 0, 7]))
-        self.assertEqual(list_1 + simple_list, CustomList([6, 3, 6, 7]))
-        self.assertEqual(simple_list - list_1, CustomList([-4, 1, 0, -7]))
-        self.assertEqual(simple_list + list_1, CustomList([6, 3, 6, 7]))
+    def test_add_customs(self):
+        init_list_1 = [5, 1, 3, 7]
+        init_list_2 = [1, 2, 7]
+        list_1 = CustomList(init_list_1)
+        list_2 = CustomList(init_list_2)
+
+        # сложение
+        self.assertTrue(list_comparison(list_1 + list_2,
+                                        CustomList([6, 3, 10, 7])))
+        self.assertTrue(list_comparison(list_2 + list_1,
+                                        CustomList([6, 3, 10, 7])))
+
+        # вычитание
+        self.assertTrue(list_comparison(list_1 - list_2,
+                                        CustomList([4, -1, -4, 7])))
+        self.assertTrue(list_comparison(list_2 - list_1,
+                                        CustomList([-4, 1, 4, -7])))
+
+        # проверка на неизменяемость
+        self.assertTrue(list_comparison(list_1,
+                                        CustomList(init_list_1)))
+        self.assertTrue(list_comparison(list_2,
+                                        CustomList(init_list_2)))
+
+    def test_add_simple_and_custom(self):
+        init_list = [6, 2, 4, 3]
+        custom_list = CustomList(init_list)
+        simple_list = [5, 1, 18]
+
+        # сложение
+        self.assertTrue(list_comparison(custom_list + simple_list,
+                                        CustomList([11, 3, 22, 3])))
+        self.assertTrue(list_comparison(simple_list + custom_list,
+                                        CustomList([11, 3, 22, 3])))
+
+        # вычитание
+        self.assertTrue(list_comparison(custom_list - simple_list,
+                                        CustomList([1, 1, -14, 3])))
+        self.assertTrue(list_comparison(simple_list - custom_list,
+                                        CustomList([-1, -1, 14, -3])))
+
+        # проверка на неизменяемость
+        self.assertTrue(list_comparison(custom_list, init_list))
+        self.assertTrue(list_comparison(simple_list, [5, 1, 18]))
 
     @patch('builtins.print')
     def test_main(self, mock_print):
-        with patch('main.__name__', '__main__'):
+        with patch('CustomList.__name__', '__main__'):
             self.assertEqual(main(), 'end of main')
-        with patch('main.__name__', '__not_main__'):
+        with patch('CustomList.__name__', '__not_main__'):
             self.assertEqual(main(), 'just end')
 
 
