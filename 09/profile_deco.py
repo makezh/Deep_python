8fd548714e87d8b2957c8da34931819e6fbd0b84
makezh <max.kezhaev@gmail.com>
2022-11-24 22:27:46 +0300

    add calls count decorator

--- 09/profile_deco.py
+++ 09/profile_deco.py
@@ -1,24 +1,35 @@
-def profile_deco(func):
-    def helper(*args, **kwargs):
-        helper.print_stat += 1
-        return func(*args, **kwargs)
+class ProfileDeco:
+    def __init__(self, function):
+        self.func = function
+        self.func.calls = 0
 
-    helper.print_stat = 0
-    helper.__name__ = func.__name__
-    return helper
+    def __call__(self, *args, **kwargs):
+        self.func.calls += 1
+        return self.func(*args, **kwargs)
 
-
-@profile_deco
-def add(a, b):
-    return a + b
+    def print_stat(self):
+        print(f"{self.func.__name__}() вызвана {self.func.calls} раз")
 
 
-@profile_deco
-def sub(a, b):
-    return a - b
+@ProfileDeco
+def add(first, second):
+    return first + second
 
 
-add(1, 2)
-add(4, 5)
+@ProfileDeco
+def sub(first, second):
+    return first - second
 
-print(add.print_stat)
+
+def main():
+    add(1, 2)
+    add(4, 5)
+    sub(4, 2)
+    add(3, 6)
+
+    add.print_stat()
+    sub.print_stat()
+
+
+if __name__ == "__main__":
+    main()
